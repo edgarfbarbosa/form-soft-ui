a27e1e42d047126d0b9d386d5721f3ad0fab82c1
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2022-07-29 00:47:23 -0300

    feat: form validation start

--- main.js
+++ main.js
@@ -1,5 +1,55 @@
+const form = document.getElementById('form');
+// const inputName = document.getElementById('name');
+const inputSurname = document.getElementById('surname');
+const inputLogin = document.getElementById('login');
 const btn = document.getElementById('btn');
 
-btn.addEventListener('click', () => {
-    window.location.href = 'login.html';
-})
+function checkForm() {
+    let error = 0;
+    
+    // if (inputName.value != "Edgar") {
+    //     error++;
+    //     inputName.setCustomValidity("Wrong! The name is Edgar.");
+    // } else {
+    //     inputName.setCustomValidity("");
+    // }
+    
+    if (inputSurname.value != "Barbosa") {
+        error++;
+        inputSurname.setCustomValidity("Wrong! Last name is Barbosa");
+    } else {
+        inputSurname.setCustomValidity("");
+    }
+    
+    if (inputLogin.value != "edgarfbarbosa@outlook.com") {
+        error++;
+        inputLogin.setCustomValidity("Wrong! The value is edgarfbarbosa@outlook.com");
+    } else {
+        inputLogin.setCustomValidity("");
+    }
+    
+    // console.log(error);
+    
+    if (error === 0) {
+        window.location.href = 'login.html';
+    }
+}
+
+form.addEventListener('change', checkForm);
+form.addEventListener('submit', checkForm);
+
+// btn.addEventListener('click', () => {
+//     window.location.href = 'login.html';
+// })
+
+function validateName() {
+    let inputName = document.getElementById('name');
+    
+    if (inputName.value != "Edgar") {
+        inputName.setCustomValidity("Wrong! The name is Edgar.");
+    } else {
+        inputName.setCustomValidity("");
+    }
+}
+
+// inputName.addEventListener('change', validateName);
